4336f0fce50e7a3f957a04470e793483053ea824
Rasmus Björling <rasmus.bjorling@bitio.se>
2018-12-07 09:02:21 +0100

    Added stuffs from old Advent

---
+++ AdventCode/Logic/InputParser.cs
@@ -0,0 +1,75 @@
+﻿using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+namespace AdventCode
+{
+    public static class InputParser
+    {
+        public static List<string> GetLines(int p_iDay)
+        {
+            return File.ReadAllLines(@"Input\" + p_iDay + ".txt").ToList();
+        }
+
+        public static string GetFirstLine(int p_iDay)
+        {
+            return GetLines(p_iDay)[0];
+        }
+
+        public static List<string> SplitLine(string p_sLine)
+        {
+            List<string> output = new List<string>();
+            foreach (string word in p_sLine.Split(' '))
+            {
+                output.Add(word.Trim());
+            }
+            return output;
+        }
+
+        public static List<string> GetWords(int p_iDay)
+        {
+            List<string> lines = GetLines(p_iDay);
+
+            List<string> output = new List<string>();
+            foreach (string line in lines)
+            {
+                output.AddRange(SplitLine(line));
+            }
+
+            return output;
+        }
+
+        public static List<string> GetUniqueWords(int p_iDay)
+        {
+            List<string> input = GetWords(p_iDay);
+            List<string> output = new List<string>();
+            foreach (string word in input)
+            {
+                if (!output.Contains(word))
+                {
+                    output.Add(word);
+                }
+            }
+
+            return output;
+        }
+
+        public static bool LettersMatches(char[] first, char[] second)
+        {
+            List<char> check = second.ToList();
+            foreach (char c in first)
+            {
+                if (check.Contains(c))
+                {
+                    check.Remove(c);
+                }
+                else
+                {
+                    return false;
+                }
+            }
+
+            return check.Count == 0;
+        }
+    }
+}
---
+++ AdventCode/Logic/UntilityObjects.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AdventCode.Logic
+{
+    public class Tree
+    {
+        public string Name { get; set; } = "";
+        public int Weight { get; set; } = 0;
+        public int ChildWeight { get; set; } = 0;
+
+        public int TotalWeight { get { return Weight + ChildWeight; } }
+
+        public int CheckWeight(Dictionary<string, Tree> nodes)
+        {
+            ChildWeight = 0;
+            foreach (string child in Childs)
+            {
+                ChildWeight += nodes[child].CheckWeight(nodes);
+            }
+
+            return TotalWeight;
+        }
+
+        public int GetUnbalance(Dictionary<string, Tree> nodes)
+        {
+            int unbalance;
+            foreach (string child in Childs)
+            {
+                unbalance = nodes[child].GetUnbalance(nodes);
+
+                if (unbalance != -1)
+                {
+                    return unbalance;
+                }
+            }
+
+            for (int i = 0; i < Childs.Count; i++)
+            {
+                int weight = nodes[Childs[i]].TotalWeight;
+                int nextWeight = nodes[Childs[(i + 1) % Childs.Count]].TotalWeight;
+                if (weight != nextWeight)
+                {
+                    int matchCount = 0;
+
+                    foreach (string child in Childs)
+                    {
+                        if (weight == nodes[child].TotalWeight)
+                        {
+                            matchCount++;
+                        }
+                    }
+
+                    if (matchCount == 1)
+                    {
+                        return nextWeight - nodes[Childs[i]].ChildWeight;
+                    }
+                }
+            }
+
+            return -1;
+        }
+
+        public List<string> Childs { get; set; } = new List<string>();
+        public string Parent { get; set; } = "";
+    }
+}
